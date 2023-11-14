78feb200ef58194c8b95a496cef2674f8c635312
Pedro Diaz <pdiaz.btm@gmail.com>
2023-11-14 14:54:05 -0400

    add key

--- src/components/ParticipantFlowModal.js
+++ src/components/ParticipantFlowModal.js
@@ -176,7 +176,7 @@ export default function ParticipantFlowModal({ folderName, product, participant,
                 <select className="bg-gray-100 p-2 my-4 rounded" id="paymentMethod" name="paymentMethod" onChange={handleInputChange} value={formData.paymentMethod}>
                     <option value="" >Selecciona una opcion</option>
                     {paymentMethods.map(paymentMethod => (
-                        <option value={`${paymentMethod.name}`}>
+                        <option key={value} value={`${paymentMethod.name}`}>
                             {paymentMethod.name}
                         </option>
                     ))}
